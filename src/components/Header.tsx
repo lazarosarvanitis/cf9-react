a93a07da6466d34704c7dbaf8556667c1a87ab4f
Lazaros Arvanitis <lazarosarvanitis@gmail.com>
2026-06-06 10:41:29 +0300

    feat: Add Products link to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router";
 const Header = () => {
   return (
     <>
-      <header className="bg-cf-dark-red fixed w-full">
+      <header className="bg-cf-dark-red fixed w-full z-50">
         <div className="container mx-auto px-4 flex items-center justify-between">
           <Link to="/">
             <img src="https://codingfactory.aueb.gr/sites/all/themes/cf_theme/logo.png"
@@ -11,6 +11,7 @@ const Header = () => {
           </Link>
           <nav className="flex gap-4 text-white font-medium">
             <Link to="/">Home</Link>
+            <Link to="/products">Products</Link>
           </nav>
         </div>
       </header>
