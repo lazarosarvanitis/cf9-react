ee96489691f61f0010ebf5db2e63d351f71dc86a
Lazaros Arvanitis <lazarosarvanitis@gmail.com>
2026-06-06 10:41:39 +0300

    feat: Add Button component to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,10 @@
+import {Button} from "../components/ui/button.tsx";
 
 const HomePage = () => {
   return (
     <>
         <h1 className="text-center">Home Page</h1>
+      <Button>Button</Button>
     </>
   )
 }
